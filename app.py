ea197579fb13407fd46853014f0b72eb4d249186
yashi2912 <yashishukla2912@gmail.com>
2026-03-19 21:35:12 +0530

    website ready without css js img linking

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+# from flask import Flask
+
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+
+# if __name__ == '__main__':
+#     app.run()
+
+
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-
-if __name__ == '__main__':
-    app.run()
+    return render_template('index.html')    
